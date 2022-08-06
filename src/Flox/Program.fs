a92eb5ce41fa46d6de56fdd421653354194f6f93
Marshall Bowers <elliott.codes@gmail.com>
2022-08-06 10:53:32 -0400

    4.5: Add basic scanner

--- src/Flox/Program.fs
+++ src/Flox/Program.fs
@@ -1,2 +1,47 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Flox.Program
+
+open System
+open System.IO
+open System.Text
+
+open Flox.Scanner
+
+let mutable hadError = false
+
+let run source =
+    let scanner = Scanner(source)
+    let tokens = scanner.ScanTokens()
+
+    for token in tokens do
+        printfn "%A" token
+
+let runFile path =
+    let bytes = File.ReadAllBytes(path)
+    run <| Encoding.UTF8.GetString(bytes)
+    if hadError then exit 65
+
+let runPrompt () =
+    let mutable quitPrompt = false
+    while not quitPrompt do
+        printf "> "
+        let line = Console.ReadLine()
+        if String.IsNullOrEmpty line then quitPrompt <- true
+        else
+            run line
+            hadError <- false
+
+let report line where message =
+    printfn "[line %i] Error%s: %s" line where message
+
+let error line message =
+    report line "" message
+
+[<EntryPoint>]
+let main args =
+    match args.Length with
+    | 0 -> runPrompt ()
+    | 1 -> runFile (args.[0])
+    | _ ->
+        printfn "Usage: flox [script]"
+        exit 64
+    0
---
+++ src/Flox/Scanner.fs
@@ -0,0 +1,120 @@
+module Flox.Scanner
+
+open System
+open System.Collections.Generic
+
+type TokenType =
+    // Single-character tokens.
+    | LeftParen
+    | RightParen
+    | LeftBrace
+    | RightBrace
+    | Comma
+    | Dot
+    | Minus
+    | Plus
+    | Semicolon
+    | Slash
+    | Star
+
+    // One of two character tokens.
+    | Bang
+    | BangEqual
+    | Equal
+    | EqualEqual
+    | Greater
+    | GreaterEqual
+    | Less
+    | LessEqual
+
+    // Literals.
+    | Identifier
+    | String
+    | Number
+
+    // Keywords.
+    | And
+    | Class
+    | Else
+    | False
+    | Fun
+    | For
+    | If
+    | Nil
+    | Or
+    | Print
+    | Return
+    | Super
+    | This
+    | True
+    | Var
+    | While
+
+    | Eof
+
+type Token =
+    {
+        Type: TokenType
+        Lexeme: string
+        Literal: Object
+        Line: int
+    }
+
+type Scanner(source: string) =
+    let tokens = List<Token>()
+
+    let mutable start = 0
+    let mutable current = 0
+    let mutable line = 1
+
+    let addToken ty literal =
+        let token =
+            {
+                Type = ty
+                Lexeme = source.Substring(start, current - start)
+                Literal = literal
+                Line = line
+            }
+
+        tokens.Add(token)
+
+    let addToken' ty = addToken ty null
+
+    let advance () =
+        let char = source.[current]
+        current <- current + 1
+        char
+
+    let scanToken () =
+        match advance () with
+        | '(' -> addToken' LeftParen
+        | ')' -> addToken' RightParen
+        | '{' -> addToken' LeftBrace
+        | '}' -> addToken' RightBrace
+        | ',' -> addToken' Comma
+        | '.' -> addToken' Dot
+        | '-' -> addToken' Minus
+        | '+' -> addToken' Plus
+        | ';' -> addToken' Semicolon
+        | '*' -> addToken' Star
+        | _ -> ()
+
+    let isAtEnd () =
+        current >= source.Length
+
+    member _.ScanTokens() =
+        while not <| isAtEnd () do
+            start <- current
+            scanToken ()
+
+        let eofToken =
+            {
+                Type = Eof
+                Lexeme = ""
+                Literal = null
+                Line = line
+            }
+
+        tokens.Add(eofToken)
+        
+        tokens
